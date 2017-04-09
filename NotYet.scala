19d244da0c392ac3bce24ee518dab1e77f1d52fd
drexly <drexly.com@gmail.com>
2017-04-09 21:10:01 +0900

    Global HotPle
    SeoulHot=소프트웨어중심대학연합해커톤수상,HotPleBot=GDGKR파이어베이스해커톤수상,HotPle=애드몹

---
+++ NotYet.scala
@@ -0,0 +1,6 @@
+object NotYet {
+  def main(args: Array[String]) {
+    println("The source code of this project has not been opened publicly yet, due to ongoing google admob contest's global version")
+  }
+}
+
